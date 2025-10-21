2514b26de3bfdb97d34c089febcb0938d9f18dac
brunoestrella <brunogamer2002@gmail.com>
2025-10-21 11:17:52 -0300

    <feat: added reviews admin panel>

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+class ReviewAdimin(admin.ModelAdmin):
+    list = ('book', 'stars', 'comment', )
+
+
+admin.site.register(Review, ReviewAdimin)
