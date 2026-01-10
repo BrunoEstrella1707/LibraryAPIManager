ed2327499e186e3c02ad06cbdbfe39d7efa68785
brunoestrella <brunogamer2002@gmail.com>
2026-01-10 18:11:44 -0300

    <fix: wish and read lists are now registered in admin page>

--- wishreadlist/admin.py
+++ wishreadlist/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import WishReadList
-# Register your models here.
 
 
 class WishReadListAdmin(admin.ModelAdmin):
     list_display = ('user', 'book', 'list_type', 'date', )
 
 
-admin.register(WishReadList, WishReadListAdmin)
+admin.site.register(WishReadList, WishReadListAdmin)
