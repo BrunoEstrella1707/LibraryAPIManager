998bd4587bb1c7daeb3d97a445e6e02194110e79
brunoestrella <brunogamer2002@gmail.com>
2026-01-18 20:18:21 -0300

    <feat: adjusts in settings>

--- app/settings.py
+++ app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': os.environ.get("DATABASE_NAME"),
         'USER': os.environ.get("DATABASE_USER"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        'HOST': os.environ.get("DATABASE_HOST"),
+        'HOST': "library_db",
         'PORT': '5432'
     },
     'dev': {
